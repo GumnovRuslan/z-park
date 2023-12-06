5bbc623432b4574d551bf13c775e7e9aa6a81905
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-12-06 11:17:56 +0300

    changed the order button

--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -1,11 +1,5 @@
 <script>
-    import BookHoliday  from "$lib/component/popup/BookHoliday.svelte";
     import { onMount } from "svelte";
-    import CustomBtn from "../component/CustomBtn.svelte";
-
-    function handleClick() {
-        window.bookHoliday.showModal()
-    }
 
     onMount(() => {
         let links = document.querySelectorAll('.link')
@@ -13,26 +7,26 @@
     })
 
     function closeMenu(links) {
-        links.forEach(link => link.addEventListener('click', () => window.falseCheckbox.checked = false))
+        links.forEach(link => link.addEventListener('click',
+        () => window.falseCheckbox.checked = false))
     }
 </script>
 
 <header class="header">
-    <input type='checkbox' id="falseCheckbox">
+    <input class="burger-checkbox" type='checkbox' id="falseCheckbox">
+
     <label class='burger' for="falseCheckbox">
         <span class="burger__line burger__line-top"></span>
         <span class="burger__line burger__line-center"></span>
         <span class="burger__line burger__line-bottom"></span>
     </label>
+
     <a class="logo-link logo-top link" href='/'>
         <img src='img/logo-removebg-cut.png' alt='' class='logo'>
     </a>
-    <nav class='header__menu' id="menu">
-        <div class="header__info">
-            <CustomBtn text='Заказать' {handleClick}/>
-            <BookHoliday />
-        </div>
-        <nav class="header__nav">
+
+    <div class='header__menu' id="menu">
+        <div class="header__nav">
             <a class="header__link link" href='/about'>О нас</a>
             <a class="header__link link" href='/holidays'>Праздники</a>
             <a class="header__link link" href='/photo'>Фото</a>
@@ -42,15 +36,13 @@
             <a class="header__link link" href='/cafe'>Кафе</a>
             <a class="header__link link" href='/prices'>Цены</a>
             <a class="header__link link" href='/contacts'>Контакты</a>
-        </nav>
-    </nav>
+        </div>
+    </div>
 </header>
 
 <style>
-    button {
-        padding: 0;
-    }
-    #falseCheckbox {
+    /* BURGER */
+    .burger-checkbox {
         display: none;
     }
     .burger {
@@ -60,7 +52,6 @@
         border: none;
         cursor: pointer;
         background: transparent;
-
     }
     .burger__line {
         width: 100%;
@@ -69,50 +60,31 @@
         background: #000;
         transition: all 0.5s;
     }
-    #falseCheckbox:checked ~ .burger .burger__line-top {
+    .burger-checkbox:checked ~ .burger .burger__line-top {
         transform: translateY(12px) rotate(45deg);
     }
-    #falseCheckbox:checked ~ .burger  .burger__line-center {
+    .burger-checkbox:checked ~ .burger .burger__line-center {
         transform: scale(0);
     }
-    #falseCheckbox:checked ~ .burger  .burger__line-bottom {
+    .burger-checkbox:checked ~ .burger .burger__line-bottom {
         transform: translateY(-12px) rotate(-45deg);
     }
-    #falseCheckbox:checked ~ .header__menu {
+    .burger-checkbox:checked ~ #menu {
         left: 0;
     }
+    /* //////////////////////// */
     .header {
-        position: sticky;
-        top: 0;
-        z-index: 10;
+        position: relative;
         box-shadow: 0 0 10px 1px #858585;
         background: #24b3ff;
     }
     .header__menu {
         transition: all 0.5s;
     }
-    .header__info {
-        display: flex;
-        justify-content: center;
-    }
-
-
-    .btn {
-        width: 200px;
-        text-decoration: none;
-        text-transform: uppercase;
-        font-weight: 700;
-        font-size: 14px;
-        border: none;
-        border-radius: 5px;
-        color: #f5f5f5;
-        background: rgb(63, 146, 89);
-    }
     .header__nav {
         display: flex;
         align-items: center;
         height: 80px;
-
     }
     .header__link {
         display: flex;
@@ -160,13 +132,6 @@
         .logo-top {
             display: none;
         }
-        .header__info {
-            gap: 20px;
-            margin-bottom: 10px;
-        }
-        .btn {
-            padding: 5px 15px;
-        }
         .header__nav {
             box-shadow: 0 0 10px #878787;
             background: #fff;
@@ -209,15 +174,6 @@
         }
         .logo-bottom {
             display: none;
-        }
-        .header__info {
-            flex-wrap: wrap;
-            gap: 10px;
-            margin-bottom: 0;
-            padding-bottom: 10px;
-            border-bottom: 2px solid rgb(147, 147, 147)        }
-        .btn {
-            padding: 10px;
         }
         .header__nav {
             padding: 0;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,26 +2,49 @@
     import Header from "$lib/sections/Header.svelte";
     import Footer from "$lib/sections/Footer.svelte";
     import BtnUp from "../lib/component/BtnUp.svelte";
+    import CustomBtn from "../lib/component/CustomBtn.svelte";
+    import BookHoliday  from "$lib/component/popup/BookHoliday.svelte";
     import BreadCrumbs from "../lib/component/BreadCrumbs.svelte";
 
   let breadcrumbs = [
     { label: 'Главная', url: '/' },
     { label: 'Текущая страница', url: null }
   ];
+
+  function handleClick() {
+        window.bookHoliday.showModal()
+    };
 </script>
 
 <div class='wrapper'>
-    <Header />
+    <BookHoliday />
+    <div class='header'>
+        <Header />
+    </div>
     <main class='main'>
         <!-- <BreadCrumbs {breadcrumbs} /> -->
         <slot />
         <BtnUp />
+        <div class='btn'>
+            <CustomBtn text='Заказать праздник' {handleClick}/>
+        </div>
     </main>
     <Footer />
 </div>
 
 
 <style>
+    .header {
+        position: sticky;
+        top: 0;
+        z-index: 10;
+    }
+    .btn {
+        position: fixed;
+        z-index: 2;
+        bottom: 10px;
+        right: 10px;
+    }
     :global(button) {
         cursor: pointer;
     }
