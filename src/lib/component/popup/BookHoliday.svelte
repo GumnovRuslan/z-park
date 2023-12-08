d1539fc4ba47b15bc192be305f94ca157b60fc3c
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-12-08 16:45:14 +0300

    change popup book holiday

--- src/lib/component/popup/BookHoliday.svelte
+++ src/lib/component/popup/BookHoliday.svelte
@@ -5,66 +5,95 @@
 
     onMount(() => {
         dialog = document.getElementById('bookHoliday')
+        dialog.addEventListener('click', (e) => closeDialog(e));
+    })
 
-        dialog.addEventListener('click', (e) => {
-            const dialogSize = dialog.getBoundingClientRect()
-            if(
-                e.clientX < dialogSize.left ||
-                e.clientX > dialogSize.right ||
-                e.clientY < dialogSize.top ||
-                e.clientY > dialogSize.bottom
-            ) {
-                dialog.close()
-            }
-        })
-    });
+    function closeDialog(e) {
+        const dialogSize = dialog.getBoundingClientRect()
+        if(e.clientX < dialogSize.left ||
+            e.clientX > dialogSize.right ||
+            e.clientY < dialogSize.top ||
+            e.clientY > dialogSize.bottom)
+            dialog.close()
+    }
+
+    function setDateNow() {
+        const addZero = (n) => n < 10 ? '0' + n : n
+        const date = new Date()
+        return `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`
+    }
 </script>
 
-<dialog class="dialog" id="bookHoliday">
-    <div class='dialog__header'>
-        <button class="dialog__close" on:click={()=>dialog.close()}>
-            <span class='dialog__inner'></span>
-        </button>
-        <p class="dialog__subtitle">заказ праздника</p>
-        <p class="dialog__description">
+<dialog class="popup" id="bookHoliday">
+    <div class='popup__header'>
+        <p class="popup__title">Заказать праздник</p>
+        <p class="popup__description">
             Оставьте заявку на проведение дня рождения и наши менеджеры свяжутся с вами и помогут с организацией.
         </p>
     </div>
-    <form class="dialog__form" method="post">
-        <input class="dialog__input" type="text" placeholder="Имя" >
-        <input class="dialog__input" type='tel' placeholder="Телефон" >
-        <input class="dialog__input" type="date" placeholder="Дата праздника" >
-        <button class="dialog__btn" type='submit'>Отправить</button>
-        <button class='dialog__btn' formmethod="dialog" type="submit" >Закрыть</button>
-    </form>
+    <div class='popup__content'>
+        <div class='popup__image-container'>
+            <img class="popup__image" src='img/woman.png' alt='manager'>
+        </div>
+        <form class="form" method="post">
+            <label class='form__label'>Имя
+                <input class="form__input" type="text" placeholder="Евгения" required>
+            </label>
+            <label class='form__label'>Телефон
+                <input class="form__input" type='tel' placeholder="+375 (XX) XXX XX XX" required>
+            </label>
+            <label class='form__label'>Дата проведения
+                <input class="form__input" type="date" value={setDateNow()} min={setDateNow()} required>
+            </label>
+            <label class='form__label-confirm'>
+                <input type='checkbox' class='form__input-confirm' required>
+                Нажимая на кнопку, я принимаю условия соглашения.
+            </label>
+            <button class="form__btn-send" type='submit'>Отправить</button>
+            <button class="popup__btn-close" type="button" on:click={dialog.close()}>
+                <span class='popup__close-line'></span>
+            </button>
+        </form>
+    </div>
 </dialog>
 
 <style>
     dialog::backdrop {
         background: #00000071;
     }
-    :global(.dialog__header) {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .popup {
+        border: 2px solid #3e40d0;
+        border-radius: 20px;
+        box-shadow: 0 0 20px 1px #5e3ed0;
     }
-     :global(.dialog__close) {
+    .popup__header {
+        margin-bottom: 15px;
+    }
+     .popup__btn-close {
         position: absolute;
-        top: 0;
-        right: 0;
-        width: 30px;
-        height: 30px;
+        top: 5px;
+        right: 5px;
+        width: 45px;
+        height: 45px;
+        padding: 10px;
+        border: none;
+        border-radius: 50%;
         cursor: pointer;
+        background: transparent;
     }
-    :global(.dialog__inner) {
+    .popup__btn-close:focus {
+        background: #3e40d017;
+        outline: 2px solid #5e3ed0
+    }
+    .popup__close-line {
         position: relative;
         display: block;
         width: 100%;
         height: 100%;
+        transition: all 0.3s;
     }
-    :global(.dialog__inner:before,
-    .dialog__inner:after) {
+    .popup__close-line:before,
+    .popup__close-line:after {
         content: '';
         position: absolute;
         top: calc(50% - 1px);
@@ -73,52 +102,149 @@
         height: 3px;
         border-radius: 5px;
         background: #000;
+        transition: all .3s;
     }
-    :global(.dialog__inner:before) {
+    .popup__close-line:before {
         transform: rotate(45deg);
     }
-    :global(.dialog__inner:after) {
+    .popup__close-line:after {
         transform: rotate(-45deg);
     }
-    :global(.dialog__subtitle) {
-        margin-bottom: 10px;
-        padding: 0 50px;
+    .popup__btn-close:hover .popup__close-line:before,
+    .popup__btn-close:hover .popup__close-line:after,
+    .popup__btn-close:focus .popup__close-line:before,
+    .popup__btn-close:focus .popup__close-line:after {
+        background: #f85151;
+    }
+    .popup__btn-close:hover .popup__close-line,
+    .popup__btn-close:focus .popup__close-line {
+        transform: scale(120%);
+    }
+    .popup__title {
+        margin-bottom: 5px;
+        padding: 0 40px;
         text-transform: uppercase;
         font-weight: 700;
-        font-size: 24px;
+        font-size: clamp(16px, 5vw, 24px);
         text-align: center;
         line-height: 1.2;
     }
-    :global(.dialog__description) {
+    .popup__description {
         max-width: 500px;
+        margin: 0 auto;
         width: 100%;
         text-transform: uppercase;
-        font-weight: 700;
-        font-size: 14px;
+        font-weight: 500;
+        font-size: clamp(11px, 3vw, 14px);
         text-align: center;
+        line-height: 1.4;
         color: #4a4a4a;
     }
-    :global(.dialog__form) {
+    .popup__content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    }
+    .popup__image-container {
+        display: flex;
+        align-items: center;
+        width: 400px;
+    }
+    .popup__image {
+        object-fit: cover;
+        width: 100%;
+    }
+    .form {
         display: flex;
         flex-direction: column;
+    }
+    .form__label {
+        width: 100%;
+        font-weight: 500;
+        font-size: clamp(14px, 4vw, 16px);
+        line-height: 1.5;
+        padding-left: 10px;
+    }
+    .form__input {
+        width: calc(100% + 10px);
+        margin-left: -10px;
+        padding: 5px 10px;
+        height: 45px;
+        outline: 1px solid #5e3ed0;
+        border: none;
+        border-radius: 5px;
+        font-family: Rubik, sans-serif;
+        font-size: clamp(16px, 4vw, 18px);
+        line-height: 1.3;
+        color: #505050;
+        transition: box-shadow 0.3s;
+    }
+    .form__input::placeholder {
+        color: #9a9a9a;
+    }
+    .form__input:hover, .form__input:focus  {
+        outline: 2px solid #5e3ed0;
+    }
+    .form__input:focus  {
+        box-shadow: 0 0 6px #3e40d0;
+        background: #3e40d017;
+    }
+    .form__label-confirm {
+        display: flex;
+        gap: 8px;
         align-items: center;
-        gap: 15px;
+        margin: 5px 0;
+        font-size: clamp(12px, 3vw, 14px);
+        font-weight: 500;
+        line-height: 1.2;
+        color: #505050;
     }
-    :global(.dialog__input) {
-        width: 100%;
-        padding: 15px;
-        border: 1px solid #000;
-        border-radius: 5px;
-        font-size: 16px;
+    .form__input-confirm {
+        min-width: 15px;
+        height: 15px;
+        margin: 0;
     }
-    :global(.dialog__btn) {
+    .form__btn-send {
+        align-self: center;
+        text-transform: uppercase;
         width: 100%;
-        padding: 10px;
-        border-radius: 5px;
-        border: 1px solid #000;
-        font-size: 20px;
+        height: 45px;
+        padding: 5px 20px;
+        font-size: clamp(14px, 4vw, 18px);
         font-weight: 700;
+        border: 2px solid #5e3ed0;
+        border-radius: 5px;
         color: #f5f5f5;
-        background: #24b3ff;
+        background: #5e3ed0;
+        transition: all .3s;
+    }
+    .form__btn-send:hover, .form__btn-send:focus {
+        background: #f5f5f5;
+        color: #5e3ed0;
+        outline: none;
+    }
+    .form__btn-send:focus {
+        box-shadow: 0 0 8px #3e40d0;
+    }
+    @media screen and (min-width: 650px) {
+        .popup {
+            padding: 20px;
+        }
+        .form {
+            gap: 10px;
+        }
+    }
+    @media screen and (max-width: 650px) {
+        .popup {
+            padding: 15px;
+        }
+        .popup__image-container {
+            display: none;
+        }
+        .form {
+            width: 100%;
+            gap: 5px;
+        }
     }
 </style>
