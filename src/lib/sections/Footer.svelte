19b4ccd12d2050476c02782070730367c81318eb
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-12-12 09:23:52 +0300

    change footer

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -2,38 +2,35 @@
     import Network from "../component/Network.svelte"
 </script>
 
-<footer class="footer">
-    <div class="footer__container">
-        <div class='footer__block-first'>
-            <div class='footer__links'>
-                <p class='footer__links-title title'>Компания</p>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
+<div class='wrapper'>
+    <div class='container'>
+        <div class='footer-top'>
+            <div class='footer-top__content'>
+                <a href='/' class='footer-top__item'>
+                    <img src='img/logo-removebg-cut.png' alt='' class='logo'>
+                </a>
+                <a href='#' class='footer-top__item'>Правила посещения</a>
+                <a href='#' class='footer-top__item'>Политика кафиденциальности</a>
             </div>
-            <div class='footer__links'>
-                <p class='footer__links-title title'>Компания</p>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
-                <a href='#' class='footer__link'>link</a>
+            <div class='footer-top__content'>
+                <a href='#' class='footer-top__item'>Контакты</a>
+                <a href='#' class='footer-top__item'>О нас</a>
+                <a href='#' class='footer-top__item'>О компании</a>
+                <a href='#' class='footer-top__item'>Цены</a>
+            </div>
+            <div class='footer-top__content'>
+                <a href='#' class='footer-top__item'>Контакты</a>
+                <a href='#' class='footer-top__item'>О нас</a>
+                <a href='#' class='footer-top__item'>О компании</a>
+                <a href='#' class='footer-top__item'>Цены</a>
             </div>
         </div>
-        <div class='footer__block-second'>
-            <p class='footer__network title'>
-                Следите за нами:
-                <Network />
-            </p>
-            <div class='footer__email'>
-                <p class='footer__email-title title'>Будьте в курсе наших акций и интересных событий:</p>
-                <input class="footer__input" type='text' placeholder="Введите ваш Email">
-                <label class="footer__text"><input type='checkbox'>ДАЮ СОГЛАСИЕ НА ОБРАБОТКУ МОИХ ПЕРСОНАЛЬНЫХ ДАННЫХ</label>
-            </div>
-            <p class='footer__number'>+375 xx xx-xx-xx</p>
+        <div class='footer-bottom'>
+            <p class='footer-bottom__copy'>@copy 2023г</p>
         </div>
     </div>
-</footer>
+</div>
+
 
 <style>
     :root {
@@ -41,92 +38,51 @@
         --text: #f5f5f5;
         --link: #d7d7d7;
     }
-    .footer__container {
+    .wrapper {
+        padding: 0 clamp(20px, 4vw, 100px);
+        background: #24b3ff;
+    }
+    .container {
+        max-width: 1440px;
+        padding: 50px 0;
+        margin: 0 auto;
+    }
+    .footer-top {
         display: flex;
         flex-wrap: wrap;
-        justify-content: space-around;
-        gap: 20px;
-        padding: 10px;
-        box-shadow: 0 0 10px #858585;
-        background: var(--bg, #24b3ff);
+        justify-content: space-between;
+        gap: clamp(5px, 1vw, 40px);
+        margin-bottom: 40px;
     }
-    .footer__block-first {
-        flex: auto;
-        display: flex;
-        justify-content: space-around;
-        gap: 20px;
-    }
-    .footer__block-second {
+    .footer-top__content {
         display: flex;
         flex-direction: column;
-        gap: 10px;
+        justify-content: space-evenly;
+        gap: 3px;
     }
-    .footer__links {
-        display: flex;
-        flex-direction: column;
+    .logo {
+        width: 120px;
     }
-    .title {
-        text-transform: uppercase;
-        font-weight: 700;
-        color: var(--text, #f5f5f5);
-    }
-    .footer__link {
+    .footer-top__item {
+        padding: 3px 0;
         text-decoration: none;
-        text-transform: uppercase;
-        font-weight: 700;
-        color: var(--link, #d7d7d7);
-        cursor: pointer;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 1.3;
+        color: #dddddd;
+        transition: all 0.2s;
     }
-    .footer__link:hover {
-        color: var(--text, #f5f5f5);
+    .footer-top__item:hover {
+        color: #fff;
     }
-    .footer__network {
-        display: flex;
-        gap: 10px;
+    .footer-bottom__copy {
+        text-align: center;
+        font-size: 14px;
+        color: #505050;
     }
-    .footer__email {
-        display: flex;
-        flex-direction: column;
-        gap: 6px;
-    }
-    .footer__input {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        border-radius: 8px;
-    }
-    .footer__text {
-        display: block;
-        font-weight: 700;
-        color: var(--text, #f5f5f5);
-    }
-    .footer__number {
-        font-weight: 700;
-        color: var(--text, #f5f5f5);
-    }
-    @media screen and (min-width: 500px) {
-        .title,
-        .footer__link {
-            font-size: 16px;
-        }
-        .footer__number {
-            font-size: 20px;
-        }
-        .footer__text {
-            font-size: 12px;
-        }
-    }
-    @media screen and (max-width: 500px) {
-        .title,
-        .footer__link {
-            font-size: 14px;
-        }
-        .footer__text {
-            font-size: 10px;
-        }
-        .footer__number {
-            text-align: center;
-            font-size: 18px;
+    @media screen and (max-width: 600px) {
+        .footer-top {
+            flex-direction: column;
         }
     }
 </style>
