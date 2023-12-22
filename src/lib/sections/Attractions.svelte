77d6841b67a9c71bd645fcb515d92f90ce20d17d
Gumnov Ruslan <ruslang.crocode@gmail.com>
2023-12-22 16:05:57 +0300

    change section attractions

--- src/lib/sections/Attractions.svelte
+++ src/lib/sections/Attractions.svelte
@@ -1,169 +1,89 @@
+<script>
+    let arr = [
+        'Поролоновая яма',
+        'Батуты',
+        'Зона для малышей',
+        'Тарзанка', 'Лабиринт',
+        'Песочница',
+        'Бассейн с шариками',
+        'Кульбитные подушки',
+        'Ледяная горка',
+        'Аэрохокей'
+        ];
+</script>
 
-<div class='container'>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Поролоновая яма</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Батуты</p>
-            <p class='text'>Детские и взрослые батуты</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>День рождения</p>
-            <p class='text'>Подарите своим детям незабываемые впечатления!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Зона для малышей</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Тарзанка</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Лабиринт</p>
-            <p class='text'>Детские и взрослые батуты</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Песочница</p>
-            <p class='text'>Подарите своим детям незабываемые впечатления!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Бассейн с шариками</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Кульбитные подушки</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Башня</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Ледяная горка</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
-    </div>
-    <div class='card'>
-        <img src='img/svg/clock.svg' alt='' class='img'>
-        <div class='content'>
-            <p class='title'>Аэрохокей</p>
-            <p class='text'>Запрыгивай прямо со скалодрома или батута!</p>
-        </div>
+<div class="attractions">
+    <div class='attractions__cards'>
+
+        {#each arr as title }
+            <div class='card'>
+                <p class='card__title'>{title}</p>
+                <div class='card__price'>от 130 BYN</div>
+                <div class='card__img-container'>
+                    <img src='/img/photo1.jpg' alt='' class='card__img'>
+                </div>
+            </div>
+        {/each}
+
     </div>
 </div>
 
 <style>
-    .container {
+    .attractions {
+        padding: 0 20px;
+    }
+    .attractions__cards {
         display: grid;
-        grid-template-columns: repeat(3, minmax(300px, 350px));
-        grid-template-rows: repeat(4, 1fr);
-        justify-items: center;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 280px));
         justify-content: center;
         gap: 20px;
     }
     .card {
-        /* flex: 40%; */
+        position: relative;
         display: flex;
-        /* border: 1px solid #000; */
-        gap: 20px;
-        /* width: 400px; */
-        /* min-height: 150px; */
+        flex-direction: column;
+        height: 200px;
         padding: 15px;
-        border-radius: 5px;
-        box-shadow: 2px 2px 5px #a7a7a7;
-        background: #e2e1ff;
+        border-radius: 20px;
+        box-shadow: 2px 2px 5px grey;
+        overflow: hidden;
     }
-    .content {
+    .card:hover .card__img {
+        transform: scale(110%);
+    }
+    .card__title {
+        flex: auto;
+        text-transform: uppercase;
+        font-size: 18px;
+        font-weight: 700;
         line-height: 1.3;
     }
-    .img {
-        width: 50px;
-        height: 50px;
-    }
-    .title {
+    .card__price {
+        flex: auto;
+        justify-self: center;
         font-weight: 700;
         font-size: 20px;
+        color: orangered;
+    }
+    .card__img-container {
+        position: absolute;
+        z-index: -1;
+        right: -15%;
+        bottom: -25%;
+        width: 200px;
+        height: 200px;
+        font-weight: 700;
         text-transform: uppercase;
+        border-radius: 50%;
+        background: grey;
+        overflow: hidden;
     }
-    .text {
-        font-size: 18px;
-        font-weight: 500;
-        color: rgb(101, 101, 101);
+    .card__img {
+        display: block;
+        object-fit: cover;
+        object-position: 70%;
+        width: 100%;
+        height: 100%;
         transition: all 0.5s;
-    }
-
-    @media screen and (max-width: 980px) {
-        .container {
-            display: grid;
-            grid-template-columns: repeat(3, minmax(200px, 250px));
-            grid-template-rows: repeat(4, 1fr);
-            justify-items: center;
-            justify-content: center;
-            gap: 10px;
-        }
-        .title {
-            font-size: 16px;
-        }
-        .text {
-            font-size: 14px;
-        }
-        .img {
-            width: 30px;
-            height: 30px;
-        }
-    }
-
-    @media screen and (max-width: 650px) {
-        .container {
-            display: flex;
-            flex-wrap: wrap;
-        }
-        .card {
-            flex: auto;
-        }
-        .title {
-            font-size: 16px;
-        }
-        .text {
-            font-size: 14px;
-        }
-        .img {
-            width: 30px;
-            height: 30px;
-        }
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,31 +19,7 @@ slides={[
     'https://pivkomarket.kz/wp-content/uploads/2020/06/akcija-31-picca.jpg',
     ]}
 />
-<section class="section">
-    <p class='title'>Z-park в Витебске</p>
-    <div class='content'>
-        <div class='card'>
-            <p class='card__title'>День рождения</p>
-            <div class='p card__price'>от 130 BYN</div>
-            <div class='card__img'>Картинка</div>
-        </div>
-        <div class='card'>
-            <p class='card__title'>Свободные прыжки</p>
-            <div class='p card__price'>от 9 BYN</div>
-            <div class='card__img'></div>
-        </div>
-        <div class='card'>
-            <p class='card__title'>Фитнес на батутах</p>
-            <div class='p card__price'>от 75 BYN</div>
-            <div class='card__img'></div>
-        </div>
-        <div class='card'>
-            <p class='card__title'>Акробатика для детей</p>
-            <div class='p card__price'>от 75 BYN</div>
-            <div class='card__img'></div>
-        </div>
-    </div>
-</section>
+
 <section class='section'>
     <h2 class='title'>Развлечения</h2>
     <Attractions />
@@ -55,7 +31,7 @@ slides={[
 
 <style>
     .section {
-        padding: 20px;
+        /* padding: 20px; */
     }
     .title {
         text-transform: uppercase;
@@ -64,58 +40,7 @@ slides={[
         text-align: center;
         padding: 20px;
         line-height: 1.3;
-    }
-    .content {
-        max-width: 900px;
-        margin: 0 auto;
-        flex-wrap: wrap;
-        display: flex;
-        justify-content: center;
-        gap: 20px;
-    }
-    .card {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        width: 300px;
-        height: 230px;
-        padding: 15px;
-        border-radius: 20px;
-        box-shadow: 2px 2px 5px grey;
-        overflow: hidden;
-    }
-    .card:hover .card__img {
-        transform: scale(110%);
-    }
-    .card__title {
-        flex: auto;
-        text-transform: uppercase;
-        font-size: 18px;
-        font-weight: 700;
-    }
-    .card__price {
-        flex: auto;
-        justify-self: center;
-        font-weight: 700;
-        font-size: 20px;
-        color: orangered;
-    }
-    .card__img {
-        position: absolute;
-        z-index: -1;
-        right: -25%;
-        bottom: -30%;
-        width: 250px;
-        height: 250px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-weight: 700;
-        text-transform: uppercase;
-        font-size: 18px;
-        border-radius: 50%;
-        background: grey;
-        transition: all 0.5s;
+        margin: 0;
     }
     @media screen and (min-width: 600px) {
         .wrapper {
