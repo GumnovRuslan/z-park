3ede812c1d75b4c0299535198c05589c2f86820e
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-01-17 13:42:11 +0300

    added more information in the section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,56 +4,71 @@
 
 <SectionTitle value='О парке'/>
 
-<div class='content'>
-    <div class='description'>
-        <div class='img-container'>
-            <div class='img'></div>
-        </div>
-        <div class='info'>
-            <p class='text'>Z-PARK - это огромное пространство, на котором дети отправляются в активное путешествие, прыгают до звезд на батутах, пробираются сквозь джунгли в лабиринтах и еще многое, что позволяет ребенку выплеснуть свою энергию, а родителям с удовольствием разделить радость ребят.</p>
-            <p class='text'>Наша команда - это семья, которая понимает, что дети меняют нашу жизнь навсегда. Для нас очень важна среда, в которую погружаются юные мечтатели, - дух приключений и неограниченной свободы развлечений наполняют наш парк.</p>
-            <p class='text'>Создание именно такой среды и соответствующей атмосферы является для нас ключевой задачей.</p>
-        </div>
+<div class='about'>
+    <div class='about__section'>
+        <img src='/img/photo1.jpg' alt='' class='about__img'>
+        <p class='about__text'>Развлекательный центр Z-park - это огромное пространство, на котором дети отправляются в активное путешествие, прыгают на батутах, пробираются сквозь препятствия в лабиринтах и еще многое, что позволяет ребенку выплеснуть свою энергию, а родителям с удовольствием разделить радость ребят.</p>
+    </div>
+
+    <div class='about__section'>
+        <p class='about__text'>Наша команда - это семья, которая понимает, что дети меняют нашу жизнь навсегда. Для нас очень важна среда, в которую погружаются юные мечтатели, - дух приключений и неограниченной свободы развлечений наполняют наш парк.</p>
+        <img src='/img/photo1.jpg' alt='' class='about__img'>
+    </div>
+
+    <div class='about__section'>
+        <img src='/img/photo1.jpg' alt='' class='about__img'>
+        <p class='about__text'>Развлекательный центр Z-park поражает своими размерами, большим количеством площадок и занятий для ребят с разными увлечениями. Здесь будет занимательно и весело как детям школьного возраста, так и самым маленьким посетителям.</p>
+    </div>
+
+    <div class='about__section'>
+        <p class='about__text'>Наш центр отвечает всем требованиям безопасности. В парке предусмотрена специальная зона ожидания для родителей, откуда они смогут наблюдать за своим ребенком.</p>
+        <img src='/img/photo1.jpg' alt='' class='about__img'>
     </div>
 </div>
 
-
 <style>
-    .content {
-        padding: 20px;
+    .about {
+        padding: 0 10px;
     }
-    .description {
+    .about__section {
+        position: relative;
         display: flex;
-        gap: 20px;
-        justify-content: center;
         align-items: center;
+        gap: 20px;
+        max-width: 1000px;
+        margin: 0 auto;
+        padding-left: 10px;
+        padding-right: 10px;
+        padding-top: clamp(30px, 5vw, 50px);
+        padding-bottom: clamp(30px, 5vw, 50px);
     }
-    .img-container {
+    .about__section:not(:last-child)::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 3px;
+        border-radius: 50%;
+        background: #ffa600;
     }
-    .img {
-        width: 350px;
-        height: 250px;
-        background: grey;
+    .about__section::after {
+        bottom: 0;
     }
-    .info {
-        width: 50vw;
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
+    .about__text {
+        font-size: clamp(16px, 4vw, 20px);
+        font-weight: 500;
+        text-align: center;
+        line-height: 1.3;
     }
-    .text {
-        line-height: 1.5;
+    .about__img {
+        width: 30%;
+        min-height: 250px;
+        object-fit: cover;
     }
 
-    @media screen and (max-width: 800px) {
-        .description {
-            flex-direction: column;
-        }
-        .img {
-            width: calc(100vw - 40px);
-        }
-        .info {
-            width: auto;
+    @media screen and (max-width: 600px) {
+        .about__img {
+            display: none;
         }
     }
 </style>
