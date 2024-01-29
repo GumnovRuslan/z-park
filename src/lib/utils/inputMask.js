697149582239ded6509c2fef55b3e21d31443d0b
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-01-29 15:06:05 +0300

    change form booking

--- src/lib/utils/inputMask.js
+++ src/lib/utils/inputMask.js
@@ -50,29 +50,50 @@ export function setMaskTel(attribute) {
 
 export function setWheelNumber(attribute) {
 	const inputs = document.querySelectorAll(`[${attribute}]`);
-	inputs.forEach((input) =>
-		input.addEventListener('wheel', (e) => {
-			e.preventDefault();
-			const input = e.target;
-			const minNumber = input.min || 0;
-			const maxNumber = input.max || 99;
-			let value = input.value;
-			const step = +input.step || 1;
-			value = e.deltaY > 0 ? +input.value - step : +input.value + step;
-			input.value = value <= minNumber ? minNumber : value >= maxNumber ? maxNumber : value;
-		})
-	);
+	inputs.forEach((input) => {
+		input.addEventListener('wheel', wheelNumber);
+		input.addEventListener('input', limitationNumber);
+	});
+
+	function wheelNumber(e) {
+		e.preventDefault();
+		const input = e.target;
+		const minNumber = input.min || 0;
+		const maxNumber = input.max || 100;
+		let value = input.value;
+		const step = +input.step || 1;
+		value = e.deltaY > 0 ? +input.value - step : +input.value + step;
+		input.value = value <= minNumber ? minNumber : value >= maxNumber ? maxNumber : value;
+	}
+
+	function limitationNumber(e) {
+		let input = e.target;
+		let inputValue = input.value;
+		let maxValue = input.max || 100;
+		let minValue = input.min || 0;
+
+		if (inputValue == '') return (e.target.value = '');
+		if (inputValue.length > 1 && inputValue[0] == 0) inputValue = inputValue.slice(1);
+		if (+inputValue > +maxValue) inputValue = maxValue;
+		else if (+inputValue < +minValue) inputValue = minValue;
+		e.target.value = inputValue;
+	}
 }
 
-export function setDateNow(attribute) {
+export function setDate(attribute, min = [0, 0, 1], max = [1, 0, 1]) {
 	const addZero = (n) => (n < 10 ? '0' + n : n);
 	const date = new Date();
-	const formatDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(
-		date.getDate()
-	)}`;
+	const formatDate = (dateShift) => {
+		return `${date.getFullYear() + dateShift[0]}-${addZero(
+			date.getMonth() + 1 + dateShift[1]
+		)}-${addZero(date.getDate() + dateShift[2])}`;
+	};
 
 	if (attribute) {
 		const inputs = document.querySelectorAll(`[${attribute}]`);
-		inputs.forEach((input) => (input.min = formatDate));
-	} else return formatDate;
+		inputs.forEach((input) => {
+			input.min = formatDate(min);
+			input.max = formatDate(max);
+		});
+	} else return formatDate([0, 0, 0]);
 }
---
+++ src/lib/utils/popup.js
@@ -0,0 +1,10 @@
+export function closeBeyond(e) {
+	const elSize = e.target.getBoundingClientRect();
+	if (
+		e.clientX < elSize.left ||
+		e.clientX > elSize.right ||
+		e.clientY < elSize.top ||
+		e.clientY > elSize.bottom
+	)
+		e.target.close();
+}
