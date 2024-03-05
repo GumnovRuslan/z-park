341bdc24723f46ebe048e9053a87c0086b0627c1
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-03-05 18:10:33 +0300

    add section review

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -19,9 +19,57 @@
     {/each}
 </div>
 
+<div class='review'>
+    <h2 class='review__title'>
+        <span class="review__title-text">Наш парк стал любимым местом для семейного отдыха!</span>
+        <span class="review__title-text">Посмотрите, что говорят о нас наши гости</span>
+    </h2>
+    <div class='review__content'>
+        <div class='review__content-image'>
+            <img src='/img/review1.webp' alt='отзывы'>
+        </div>
+        <div class='review__content-image'>
+            <img src='/img/review2.webp' alt='отзывы'>
+            <img src='/img/review3.webp' alt='отзывы'>
+        </div>
+        <div class='review__content-image'>
+            <img src='/img/review4.webp' alt='отзывы'>
+        </div>
+    </div>
+</div>
+
 <style>
+    .review {
+        padding: 0 20px;
+    }
+    .review__title {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 30px;
+    }
+
+    .review__title-text {
+        text-align: center;
+        max-width: 600px;
+    }
+
+    .review__content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .review__content-image img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
     .about {
         padding: 0 10px;
+        margin-bottom: 50px;
     }
 
     .about__info-block {
@@ -29,7 +77,7 @@
         padding: 20px 0;
     }
 
-    .about__info-block:not(:last-child)::after  {
+    .about__info-block::after  {
         content: '';
         position: absolute;
         left: 0;
@@ -38,5 +86,15 @@
         height: 3px;
         border-radius: 50%;
         background: #ffa600;
+    }
+
+    @media (max-width: 780px) {
+        .review__content {
+            flex-direction: column;
+        }
+        .review__content-image {
+            display: flex;
+            max-height: 300px;
+        }
     }
 </style>
